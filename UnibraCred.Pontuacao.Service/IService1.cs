c160df51ec77f1a40a5fe0ec52769dc3e0cacf8f
michellsilva <michell32512206@hotmail.com>
2016-02-21 19:50:25 -0300

    #Service

---
+++ UnibraCred.Pontuacao.Service/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using UnibraCred.Pontuacao.Entity;
+
+namespace UnibraCred.Pontuacao.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+
+        [WebGet(ResponseFormat=WebMessageFormat.Json)]
+        [OperationContract]
+        string obterTotalPontos(string inJson);
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<PontuacaoFatura> obterTotalDetalhes(int cartaoId);
+
+        [OperationContract]
+        string pontosPorFatura(int faturaId);
+    }
+}
---
+++ UnibraCred.Pontuacao.Service/Service.svc.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Services;
+using UnibraCred.Pontuacao.Entity;
+using UnibraCred.Pontuacao.Facade;
+using System.Web.Script.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace UnibraCred.Pontuacao.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        private Fachada fachada = new Fachada();
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string obterTotalPontos(string inJson)
+        {
+
+            var json = "";
+            try
+            {
+
+                JSchema schema = JSchema.Parse(@"{
+                    'cartao_Id': {'type':'string'}
+                  }
+                }");
+
+                JSchema user = JSchema.Parse(inJson);
+                PontuacaoFatura pont = JsonConvert.DeserializeObject<PontuacaoFatura>(inJson);
+                var totalPontos = fachada.obterTotalPontos(pont.cartao_id);
+
+
+
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+                json = "{return: " + totalPontos + "}";
+
+                //json = JsonConvert.SerializeObject(fachada.obterTotalPontos(pont.cartao_id), Formatting.Indented);
+
+                // serialize JSON to a string and then write string to a file
+                File.WriteAllText(@"C:\Users\Lenovo\Documents\Pos Engenharia Software\Pontuacao2.0\Pontuacao\movie.json", JsonConvert.SerializeObject(json));
+
+                // serialize JSON directly to a file
+                using (StreamWriter file = File.CreateText(@"C:\Users\Lenovo\Documents\Pos Engenharia Software\Pontuacao2.0\Pontuacao\movie.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, json);
+                }
+                return json;
+
+            }
+            catch (FormatException ex)
+            {
+                ex.ToString();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+            return json;
+        }
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<PontuacaoFatura> obterTotalDetalhes(int cartaoId)
+        {
+            PontuacaoFatura retorno = new PontuacaoFatura();
+            return fachada.LitartodosDetalhes(cartaoId);
+            //var json = "";
+
+            //try
+            //{
+            //    JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            //    List<PontuacaoFatura> obj = fachada.LitartodosDetalhes(cartaoId);
+
+            //    json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            //    //foreach (var item in obj)
+            //    //{
+            //    //    retorno.cartao_id = item.cartao_id;
+            //    //retorno.fatura_id = item.fatura_id;
+            //    //retorno.dtInclusao = item.dtInclusao;
+            //    //retorno.pontosQtd = item.pontosQtd;
+            //    //json += JsonConvert.SerializeObject(retorno, Formatting.Indented);
+            //    //}
+
+            //    //retorno.cartao_id = obj.cartao_id;
+            //    //retorno.fatura_id = obj.fatura_id;
+            //    //retorno.dtInclusao = obj.dtInclusao;
+            //    //retorno.pontosQtd = obj.pontosQtd;
+            //    //json = JsonConvert.SerializeObject(retorno, Formatting.Indented);
+
+
+
+            //    // serialize JSON to a string and then write string to a file
+            //    File.WriteAllText(@"C:\Users\Lenovo\Documents\Pos Engenharia Software\Pontuacao2.0\Pontuacao\movie.json", JsonConvert.SerializeObject(retorno));
+
+            //    // serialize JSON directly to a file
+            //    using (StreamWriter file = File.CreateText(@"C:\Users\Lenovo\Documents\Pos Engenharia Software\Pontuacao2.0\Pontuacao\movie.json"))
+            //    {
+            //        JsonSerializer serializer = new JsonSerializer();
+            //        serializer.Serialize(file, retorno);
+            //    }
+            //    return json;
+            //}
+            //catch (Exception ex)
+            //{
+            //    ex.ToString();
+            //}
+            //return json;
+        }
+
+
+        public string pontosPorFatura(int faturaId)
+        {
+            return "não implementado";
+        }
+
+    }
+}
