2b02f477a1a336f2ae59e165e890314af5cc2217
mariogit08 <mario.chaves@live.com>
2016-02-25 20:48:38 -0300

    Subindo interfaces

---
+++ UnibraCred.Pontuacao.Persistencia/interfaces/IFaturaDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnibraCred.Pontuacao.Persistencia.basic;
+
+namespace UnibraCred.Pontuacao.Persistencia.interfaces
+{
+    public interface IFaturaDAO
+    {
+        Fatura obterFatura(int idFatura);
+    }
+}
---
+++ UnibraCred.Pontuacao.Persistencia/interfaces/ITaxaConversaoDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnibraCred.Pontuacao.Entity;
+
+namespace UnibraCred.Pontuacao.Persistencia.interfaces
+{
+    public interface ITaxaConversaoDAO
+    {
+        TaxaConversao obterTaxaConversaoAtual();
+    }
+}
