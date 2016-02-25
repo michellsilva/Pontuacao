66b34d5582fd1345c70f5055821ebdc8909fcfcb
mariogit08 <mario.chaves@live.com>
2016-02-25 15:10:41 -0300

    Subindo arquivos faltantes do projeto antigo para o novo 2.0

---
+++ UnibraCred.Pontuacao.Persistencia/basic/Fatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnibraCred.Pontuacao.Persistencia.basic
+{
+    public class Fatura
+    {
+        public int id;
+        public decimal valor;
+
+        //Necessário para busca por cartão onde não se tem uma fatura específica.
+        public int cartaoId;
+
+    }
+}
